c15dc20d6859b0bcb114031c76f0df78e14ab833
Sanjaypandi123 <sanjayelanchikani@gmail.com>
2026-02-09 14:21:51 +0530

    task completed

--- 09-02--string--task/script.js
+++ 09-02--string--task/script.js
@@ -10,10 +10,12 @@ let submit = ()=>{
     let smll_cont = document.getElementById("smll_cont")
     let char_cont = document.getElementById("char_cont")
     let num_cont = document.getElementById("num_cont")
+    let spa_cont = document.getElementById("spa_count")
 
     let c_cont = 0
     let s_cont = 0
     let sp_cont = 0
+    let space_cont = 0
     let n_cont = 0
 
 
@@ -30,10 +32,11 @@ let submit = ()=>{
         else if((total >=48 && total<=57)){
             n_cont++
         }
-        else if((total!=32)){
-            sp_cont++
+        else if((total==32)){
+            space_cont++
         }
         else{
+            sp_cont++
         } 
     }
 
@@ -41,5 +44,7 @@ let submit = ()=>{
     smll_cont.innerText=s_cont
     char_cont.innerText=sp_cont
     num_cont.innerText=n_cont
+    spa_cont.innerText=space_cont
 
+    // name.value=""
 }
